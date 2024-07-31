1bd0f21f2a68ed95ee3c86e13d7f5105274124b2
Hardik0702 <hardikgupta0710@gmail.com>
2024-07-31 20:44:20 +0530

    add it

--- js/login.js
+++ js/login.js
@@ -1,7 +1,9 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+// const login-btn=documnet.querySelector("#login-btn");
 const container = document.querySelector(".container");
-
+// var user=user1234
+// var pass=usedemoclass1
 sign_up_btn.addEventListener('click', () =>{
     container.classList.add("sign-up-mode");
 });
@@ -9,3 +11,7 @@ sign_up_btn.addEventListener('click', () =>{
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
+
+// login-btn.addEventListener('click', ()=>{
+  // if()
+// })
