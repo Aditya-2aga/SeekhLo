2cadddf640b7c67fd75acaffcaeef2dafdd779e8
Hardik0702 <hardikgupta0710@gmail.com>
2024-08-01 23:30:18 +0530

    change in js

--- js/login.js
+++ js/login.js
@@ -1,31 +1,33 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-// const login-btn =documnet.querySelector("#login-btn");
 const container = document.querySelector(".container");
-var user="user1234";
-var pass="usedemo";
-sign_up_btn.addEventListener('click', () =>{
+
+var user = "user1234";
+var pass = "usedemo";
+
+sign_up_btn.addEventListener('click', () => {
     container.classList.add("sign-up-mode");
 });
 
-sign_in_btn.addEventListener('click', () =>{
+sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 });
 
 document.getElementById("login-btn").addEventListener("click", myFunction);
 
-function myFunction() {
+function myFunction(event) {
+  event.preventDefault(); // Prevent form from submitting
+
   let inputUser = document.getElementById("myuser");
-  let inputUserValue=inputUser.value;
+  let inputUserValue = inputUser.value;
 
   let inputPassword = document.getElementById("mypassword");
-  let inputPasswordValue=inputPassword.value;
+  let inputPasswordValue = inputPassword.value;
 
-  if (inputUserValue==user && inputPasswordValue==pass){
-      // alert("Correct password");
+  if (inputUserValue === user && inputPasswordValue === pass) {
+      alert("Login successful");
       window.location.href = "index.html";
-  }
-  else{
-    alert("Enter valid user id and password");
+  } else {
+    alert("Enter a valid user id and password");
   }
 }
